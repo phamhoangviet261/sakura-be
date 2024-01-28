3128d494a5991c4210f1778a921b513315d4cdff
vietph <vietph@mogul.sg>
2024-01-28 15:57:15 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const apiProxy = createProxyMiddleware({
+    target: 'https://cuutruyent9sv7.xyz',
+    changeOrigin: true,
+    pathRewrite: {
+        
+    },
+});
+
+// Use the proxy middleware
+app.use('', apiProxy);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Proxy server is running on http://localhost:${PORT}`);
+});
